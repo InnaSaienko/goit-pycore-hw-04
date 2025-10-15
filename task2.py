68f91b6be0a387685b0737433b7007c0df5eb8dc
Inna Saienko <inna.saienko.nl@gmail.com>
2025-10-15 08:31:05 +0200

    task 2: turn map() to list comperhensions

--- task2.py
+++ task2.py
@@ -1,21 +1,21 @@
+from pathlib import Path
+
+
 def get_cats_info(path):
     try:
         with open(path, "r", encoding="utf-8") as cats:
-            cats_info= list(
-                map(
-                    lambda line: {"id": line.strip().split(',')[0],
-                                    "name": line.strip().split(',')[1],
-                                    "age": line.strip().split(',')[2]},
-                cats))
+            cats_info = list(
+                {"id": line.strip().split(',')[0],
+                 "name": line.strip().split(',')[1],
+                 "age": line.strip().split(',')[2]}
+                for line in cats)
         return cats_info
-
     except FileNotFoundError:
         print(f"Error: File with path {path} doesn't exist.")
-
     except Exception as e:
         print(f"An error occurred: {e}")
 
 
 if __name__ == "__main__":
-    cats = get_cats_info("./utils/cats_info.txt")
+    cats = get_cats_info(Path("./utils/cats_info.txt"))
     print(f"List of all cats: {cats}")
