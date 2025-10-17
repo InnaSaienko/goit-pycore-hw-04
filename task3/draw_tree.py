902498f8b93c5787d17935931a557dba942aea3c
Inna Saienko <inna.saienko.nl@gmail.com>
2025-10-17 18:42:36 +0200

    task 3: switch to depth drawing

--- task3/draw_tree.py
+++ task3/draw_tree.py
@@ -6,26 +6,30 @@ FILE_COLOR = Fore.GREEN
 DIR_COLOR = Fore.BLUE
 RESET = Style.RESET_ALL
 
-# Symbols
-VERT = '│   '
-BRANCH = '├── '
-LAST = '└── '
-SPACE = '    '
+# Graphics
+VERTICAL_LINE = '│   '
+INNER_ENTRY = '├── '
+LAST_ENTRY = '└── '
 
-def draw_tree(path: Path, depth: int = 0):
-    print(f"{DIR_COLOR}{path.name}{os.sep}{RESET}")
+
+def draw_tree(path: Path, depth: int = 1):
+    print(f"{DIR_COLOR}{path.name}{os.sep}{RESET}", end="")
+
     try:
         entries = list(path.iterdir())
-    except PermissionError:
-        print(f"{VERT}[Permission Denied]")
+    except OSError:
+        print(f" [Could not read directory contents]")
         return
 
+    print("")
+
+    last_idx = len(entries) - 1
+
     for i, entry in enumerate(entries):
-        print(f"{SPACE * depth}", end="")
+        connector = LAST_ENTRY if i == last_idx else INNER_ENTRY
+        print(f"{VERTICAL_LINE * (depth - 1)}{connector}", end="")
+
         if entry.is_dir():
-            draw_tree(entry, depth+1)
+            draw_tree(entry, depth + 1)
         else:
             print(f"{FILE_COLOR}{entry.name}{RESET}")
-
-
-
